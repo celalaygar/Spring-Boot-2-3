0537fcec8dcab917886b3826ca2bad1148aac1f4
Celal Aygar <celal.aygar@gmail.com>
2018-05-23 15:19:39 +0300

    Add files via upload

---
+++ 6-springboot-jpa-with-h2-database (REST_API_QUERY)/src/main/java/com/javaegitimleri/app/exception/OwnerNotFoundException.java
@@ -0,0 +1,10 @@
+package com.javaegitimleri.app.exception;
+
+public class OwnerNotFoundException extends RuntimeException {
+
+	public OwnerNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
