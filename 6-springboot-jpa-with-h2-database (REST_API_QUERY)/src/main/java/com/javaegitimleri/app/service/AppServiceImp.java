f943c321a7b7b76a4d956724c62bea70e1cdb635
Celal Aygar <celal.aygar@gmail.com>
2018-05-23 20:04:04 +0300

    Add files via upload

--- 6-springboot-jpa-with-h2-database (REST_API_QUERY)/src/main/java/com/javaegitimleri/app/service/AppServiceImp.java
+++ 6-springboot-jpa-with-h2-database (REST_API_QUERY)/src/main/java/com/javaegitimleri/app/service/AppServiceImp.java
@@ -5,18 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javaegitimleri.app.dao.PersonelRepository;
+import com.javaegitimleri.app.dao.PetRepository;
 import com.javaegitimleri.app.exception.OwnerNotFoundException;
 import com.javaegitimleri.app.model.Personel;
 
 @Service
-@Transactional
+@Transactional(rollbackFor=Exception.class)
 public class AppServiceImp implements AppService {
 	
 	
 	private PersonelRepository personelRepository;
+	
+	private PetRepository petrepository;
 	
 	@Autowired
 	public void setPersonelRepository(PersonelRepository personelRepository) {
@@ -24,18 +28,21 @@ public class AppServiceImp implements AppService {
 	}
 	
 	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
 	public List<Personel> findPersonels() {
 		// TODO Auto-generated method stub
 		return personelRepository.findAll();
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
 	public List<Personel> findPersonel(String firstName) {
 		// TODO Auto-generated method stub
 		return personelRepository.findByFirstName(firstName);
 	}
 
 	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
 	public Personel findPersonel(Long id) throws OwnerNotFoundException {
 		// TODO Auto-generated method stub
 		Personel personel=personelRepository.findById(id);
