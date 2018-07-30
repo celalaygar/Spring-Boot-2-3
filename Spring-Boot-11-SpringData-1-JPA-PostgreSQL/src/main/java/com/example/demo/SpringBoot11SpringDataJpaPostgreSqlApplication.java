251f7507df370b5c0bb27408bf5f884119615a04
Celal Aygar <celal.aygar@gmail.com>
2018-07-31 00:00:02 +0300

    Add files via upload

---
+++ Spring-Boot-11-SpringData-1-JPA-PostgreSQL/src/main/java/com/example/demo/SpringBoot11SpringDataJpaPostgreSqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot11SpringDataJpaPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot11SpringDataJpaPostgreSqlApplication.class, args);
+	}
+}
