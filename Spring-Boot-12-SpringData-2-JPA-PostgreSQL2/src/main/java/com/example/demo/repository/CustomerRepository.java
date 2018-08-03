8955610934ec56586d7e73802836fe4b0d33739a
Celal Aygar <celal.aygar@gmail.com>
2018-08-03 22:29:36 +0300

    Add files via upload

---
+++ Spring-Boot-12-SpringData-2-JPA-PostgreSQL2/src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
