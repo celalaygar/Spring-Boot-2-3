4ed18f9c9d4c8ef318a37bfce82bb339089425ad
Hacı Celal Aygar <celal.aygar@gmail.com>
2018-09-19 03:44:38 +0300

    Delete CustomerRolesRepository.java

--- Spring-Boot-15-SpringSecurity-(Simple)-JPA-PostgreSQL/Spring-Boot-15-SpringSecurity-(Simple)-JPA-PostgreSQL/src/main/java/com/example/demo/repository/CustomerRolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.CustomerRoles;
-
-public interface CustomerRolesRepository extends CrudRepository<CustomerRoles, Long> {
-
-}
