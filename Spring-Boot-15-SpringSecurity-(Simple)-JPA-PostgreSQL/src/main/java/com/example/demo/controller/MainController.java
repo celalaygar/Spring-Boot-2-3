a13690423488cca18a97251725a6da97ba63bf25
Hacı Celal Aygar <celal.aygar@gmail.com>
2018-12-22 01:34:11 +0300

    Update MainController.java

--- Spring-Boot-15-SpringSecurity-(Simple)-JPA-PostgreSQL/src/main/java/com/example/demo/controller/MainController.java
+++ Spring-Boot-15-SpringSecurity-(Simple)-JPA-PostgreSQL/src/main/java/com/example/demo/controller/MainController.java
@@ -48,8 +48,53 @@ public class MainController {
 	@RequestMapping("/customer/{email}")
 	public String customerByemail(@PathVariable String email) {
 		Customer customer = customerRepository.findCustomerDataByemail(email);
-		return customer.getId()+" / "+customer.getName()+" / "+customer.getEmail()+" / "+customer.getPassword();
+		String getCustomer="id : "+customer.getId()+" <br/> ";
+		getCustomer+="name : "+customer.getName()+" <br/> ";
+		getCustomer+="email : "+customer.getEmail()+"<br/> ";
+		getCustomer+="password : "+customer.getPassword()+" <br/> ";
+		return getCustomer;
 	}
+
+
+	@RequestMapping("/insert")
+	@ResponseBody
+	public String insert() {
+		Customer c1 = new Customer("FatihTerim@hotmail.com", "Fatih Terim", "Fatih34", true);
+		Customer c2 = new Customer("kahraman.Yalçın@gmail.com", "Yalçın kahraman", "kahraman06", true);
+		Customer c3 = new Customer("Kebapçı.Husnu@gmail.com", "Kebapçı Hüsnü", "Hüsnü123", true);
+		Customer c4 = new Customer("Ayça.ilgi@mynet.com", "Ayça İlgi Bilem", "ilgi-bilem", true);
+		Customer c5 = new Customer("Ayşe.ilgisiz@hotmail.com", "Ayşe ilgisiz", "00ilgisiz", true);
+		Customer c6 = new Customer("Sila.Kanat@mynet.com", "Sıla Kanata", "Sila-Ka", true);
+		Customer c7 = new Customer("Arda.Turan@hotmail.com", "Arda Turan", "Arda-GS", true);
+
+		customerRepository.save(c1);
+		customerRepository.save(c2);
+		customerRepository.save(c3);
+		customerRepository.save(c4);
+		customerRepository.save(c5);
+		customerRepository.save(c6);
+		customerRepository.save(c7);
+
+		CustomerRoles role1 = new CustomerRoles("ADMIN_ADMIN", c1);
+		CustomerRoles role2 = new CustomerRoles("ADMIN_USER", c1);
+		CustomerRoles role3 = new CustomerRoles("ADMIN_EDITOR", c1);
+		CustomerRoles role4 = new CustomerRoles("ADMIN_WRİTER", c3);
+		CustomerRoles role5 = new CustomerRoles("ADMIN_EDITOR", c2);
+		CustomerRoles role6 = new CustomerRoles("ADMIN_USER", c3);
+		CustomerRoles role7 = new CustomerRoles("ADMIN_USER", c4);
+		CustomerRoles role8 = new CustomerRoles("ADMIN_ADMIN", c5);
+		CustomerRoles role9 = new CustomerRoles("ADMIN_WRİTER", c5);
+		CustomerRoles role10 = new CustomerRoles("ADMIN_EDITOR", c6);
+		CustomerRoles role11 = new CustomerRoles("ADMIN_USER", c7);
+		CustomerRoles role12 = new CustomerRoles("ADMIN_EDITOR", c7);
+
+		// how to use save all roles of customers
+		customerRolesRepository.saveAll(
+				Arrays.asList(role1, role2, role3, role4, role5, role6, role7, role8, role9, role10, role11, role12));
+
+		return "insert Data..................";
+	}
+
 	
 	@RequestMapping("/userinfo")
 	public String userinfo(Map<String, Object> model) {
