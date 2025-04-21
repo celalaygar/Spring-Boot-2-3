50514612274fc7f6fa0cf16113da6e281d809432
Hacı Celal Aygar <celal.aygar@gmail.com>
2025-04-21 19:34:30 +0300

    Delete Spring-Boot-31-Security-MongoDB-RateLimit-Configuration directory

--- Spring-Boot-31-Security-MongoDB-RateLimit-Configuration/src/main/java/com/tracker/job_ts/auth/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.tracker.job_ts.auth.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
